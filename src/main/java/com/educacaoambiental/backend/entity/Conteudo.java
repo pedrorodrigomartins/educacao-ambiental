2d98eda93a8c7c141c4125fed9ca9b852b20d7fc
Pedro Rodrigo <pedrorodrigo@Pedros-MacBook-Air.local>
2026-03-05 17:16:29 -0300

    feat: a entidade Conteudo foi atualizado

--- src/main/java/com/educacaoambiental/backend/entity/Conteudo.java
+++ src/main/java/com/educacaoambiental/backend/entity/Conteudo.java
@@ -1,7 +1,11 @@
 package com.educacaoambiental.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+
 import lombok.Setter;
 
 import java.util.HashSet;
@@ -17,12 +21,17 @@ public class Conteudo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    @NotBlank(message = "O título é obrigatório")
     private String titulo;
 
+    @Column(nullable = false)
+    @NotNull(message = "O tipo de conteúdo é obrigatório")
     @Enumerated(EnumType.STRING)
     private TipoConteudo tipoConteudo;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JsonIgnore
     @JoinTable(
             name = "conteudo_categoria",
             joinColumns = @JoinColumn(name = "conteudo_id"),

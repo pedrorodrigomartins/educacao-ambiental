2a92971954d0ea6f2bf14eaab2b3149507ca39cb
Pedro Rodrigo <pedrorodrigo@Pedros-MacBook-Air.local>
2026-03-05 17:08:05 -0300

    feat: ConteudoRepository foi criado

--- src/main/java/com/educacaoambiental/backend/repository/ConteudoRepository.java
+++ src/main/java/com/educacaoambiental/backend/repository/ConteudoRepository.java
@@ -1,7 +1,10 @@
 package com.educacaoambiental.backend.repository;
 
+import com.educacaoambiental.backend.entity.Conteudo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ConteudoRepository {
+public interface ConteudoRepository extends JpaRepository<Conteudo, Long> {
+
 }

212dfaffc121472958a44010513048e0af165d6e
Pedro Rodrigo <pedrorodrigo@Pedros-MacBook-Air.local>
2026-03-05 17:02:04 -0300

    feat: Categoria foi atualizada

--- src/main/java/com/educacaoambiental/backend/entity/Categoria.java
+++ src/main/java/com/educacaoambiental/backend/entity/Categoria.java
@@ -1,6 +1,9 @@
 package com.educacaoambiental.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,9 +20,16 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    @NotBlank(message = "O nome da categoria é obrigatório")
+    @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
+    private String nome;
+
+    @NotBlank(message = "A descrição é obrigatória")
     private String descricao;
 
     @ManyToMany(mappedBy = "categorias")
+    @JsonIgnore
     private Set<Conteudo> conteudos = new HashSet<>();
 
 }

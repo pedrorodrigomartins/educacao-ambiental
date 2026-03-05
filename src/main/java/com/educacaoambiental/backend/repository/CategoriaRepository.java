bacd8d85dee55060a08749fa294a654c5b680877
Pedro Rodrigo <pedrorodrigo@Pedros-MacBook-Air.local>
2026-03-05 17:05:46 -0300

    feat: CategoriaRepository foi criada

---
+++ src/main/java/com/educacaoambiental/backend/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.educacaoambiental.backend.repository;
+
+import com.educacaoambiental.backend.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
---
+++ src/main/java/com/educacaoambiental/backend/repository/ConteudoRepository.java
@@ -0,0 +1,7 @@
+package com.educacaoambiental.backend.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ConteudoRepository {
+}

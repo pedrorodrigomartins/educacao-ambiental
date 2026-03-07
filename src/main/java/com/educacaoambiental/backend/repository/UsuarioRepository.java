e167cd3995af637d78fb3ad3e508ee3d6a76af37
Pedro Rodrigo <pedrorodrigo@Pedros-MacBook-Air.local>
2026-03-06 21:23:29 -0300

    feat: foi adicionada classe UsuarioRepository com o metodo findByEmail

---
+++ src/main/java/com/educacaoambiental/backend/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.educacaoambiental.backend.repository;
+
+import com.educacaoambiental.backend.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+}
